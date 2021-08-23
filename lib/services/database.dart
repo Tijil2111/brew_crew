346164324a15a6decdee434810b71a02a4c7e102
Tijil Agrawal <tijil2008@gmail.com>
2021-08-23 14:41:56 +0530

    Listing Brew Data

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,3 +1,6 @@
+
+import 'package:brew_crew/models/brew.dart';
+import 'package:brew_crew/screens/home/brew_list.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
@@ -19,10 +22,22 @@ class DatabaseService {
     });
   }
 
+// brew list from snapshot
+  List<Brew> _brewListFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.documents.map((doc){
+      return Brew(
+        name: doc.data['name'] ?? '',
+        strength: doc.data['strength'] ?? '0',
+        sugars: doc.data['sugars'] ?? 'O'
+      );
+    }).toList();
+  }
+
 
   //get brews stream
-Stream<QuerySnapshot> get brews {
-    return brewCollection.snapshots();
+Stream<List<Brew>> get brews {
+    return brewCollection.snapshots()
+    .map(_brewListFromSnapshot);
 }
 }
 
